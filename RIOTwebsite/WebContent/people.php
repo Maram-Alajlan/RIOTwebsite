3f04c444a534816cfebc1f8e11af430f1163733f
aniskoubaa <akoubaa@coins-lab.org>
2017-02-14 14:50:22 +0300

    fixing people page

--- RIOTwebsite/WebContent/people.php
+++ RIOTwebsite/WebContent/people.php
@@ -8,138 +8,231 @@
 <meta name="description" content="Responsive Website Template">
 <meta name="author" content="The Develovers">
 <!-- CORE CSS -->
-<link href="theme/assets/css/bootstrap.css" rel="stylesheet" type="text/css">
-<link href="theme/assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-<link href="theme/assets/css/elegant-icons.css" rel="stylesheet" type="text/css">
+<link href="theme/assets/css/bootstrap.css" rel="stylesheet"
+	type="text/css">
+<link href="theme/assets/css/font-awesome.min.css" rel="stylesheet"
+	type="text/css">
+<link href="theme/assets/css/elegant-icons.css" rel="stylesheet"
+	type="text/css">
 <!-- THEME CSS -->
 <link href="theme/assets/css/main.css" rel="stylesheet" type="text/css">
-<link href="theme/assets/css/my-custom-styles.css" rel="stylesheet" type="text/css">
+<link href="theme/assets/css/my-custom-styles.css" rel="stylesheet"
+	type="text/css">
 <link href="theme/assets/css/riot.css" rel="stylesheet" type="text/css">
 
 <!-- GOOGLE FONTS -->
-<link href='https://fonts.googleapis.com/css?family=Raleway:700,400,400italic,500' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Lato:400,400italic,700,300,300italic' rel='stylesheet' type='text/css'>
+<link
+	href='https://fonts.googleapis.com/css?family=Raleway:700,400,400italic,500'
+	rel='stylesheet' type='text/css'>
+<link
+	href='https://fonts.googleapis.com/css?family=Lato:400,400italic,700,300,300italic'
+	rel='stylesheet' type='text/css'>
 <!-- FAVICONS -->
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="theme/assets/ico/bravana144.png">
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="theme/assets/ico/bravana114.png">
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="theme/assets/ico/bravana72.png">
-<link rel="apple-touch-icon-precomposed" href="theme/assets/ico/bravana57.png">
+<link rel="apple-touch-icon-precomposed" sizes="144x144"
+	href="theme/assets/ico/bravana144.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114"
+	href="theme/assets/ico/bravana114.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72"
+	href="theme/assets/ico/bravana72.png">
+<link rel="apple-touch-icon-precomposed"
+	href="theme/assets/ico/bravana57.png">
 <link rel="shortcut icon" href="theme/assets/ico/favicon.ico">
 </head>
 
 <body>
-<!-- WRAPPER -->
-<div id="wrapper">
-<?php
-    include_once 'include/nav_bar.php';
-    ?>
-<!-- END NAVBAR -->
-<!-- PAGE HEADER DEFAULT -->
-<div class="page-header">
-<div class="container">
-<h1 class="page-title pull-left">People</h1>
-<ol class="breadcrumb link-accent">
-<li><a href="index.html">Home</a></li>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+	<?php
+	include_once 'include/nav_bar.php';
+	?>
+		<!-- END NAVBAR -->
+		<!-- PAGE HEADER DEFAULT -->
+		<div class="page-header">
+			<div class="container">
+				<h1 class="page-title pull-left">People</h1>
+				<ol class="breadcrumb link-accent">
+					<li><a href="index.html">Home</a></li>
 
-<li class="active">Has Navigation Links</li>
-</ol>
-</div>
-</div>
-<!-- END PAGE HEADER DEFAULT -->
-<!-- PAGE CONTENT -->
-<div class="page-content no-margin-bottom">
-<div class="container">
-<p>  </p>
+					<li class="active">Has Navigation Links</li>
+				</ol>
+			</div>
+		</div>
+		<!-- END PAGE HEADER DEFAULT -->
+		<!-- PAGE CONTENT -->
+		<div class="page-content no-margin-bottom">
+			<div class="container">
+				<p></p>
 
-<!-- TEAM -->
-<section>
-<div class="container">
-<h2 class="section-heading">Our Team</h2>
-<div class="row">
-<div class="col-sm-4">
-<div class="team-member">
-<img src="images/researcher.png"  class="img-responsive" alt="Person">
-<div class="member-info">
-<h3 class="name">Dr. Anis Koubaa</h3>
-<span class="title">Director</span>
-<p class="short-bio">Dramatically harness 2.0 products and leveraged sources. Dramatically drive intermandated expertise rather than clicks-and-mortar benefits.</p>
-<ul class="list-inline social-icons social-icons-small">
-<li><a href="#" class="facebook-bg-hover"><i class="fa fa-facebook"></i></a></li>
-<li><a href="#" class="dribbble-bg-hover"><i class="fa fa-dribbble"></i></a></li>
-<li><a href="https://www.linkedin.com/in/anis-koubaa-84aa4344/" class="linkedin-bg-hover"><i class="fa fa-linkedin"></i></a></li>
-<li><a href="#" class="twitter-bg-hover"><i class="fa fa-twitter"></i></a></li>
-</ul>
-</div>
-</div>
-</div>
-<div class="col-sm-4">
-<div class="team-member">
-<img src="images/researcher.png"  class="img-responsive" alt="Person">
-<div class="member-info">
-<h3 class="name">Dr. Basit Qureshi</h3>
-<span class="title">Senior Researcher</span>
-<p class="short-bio">Dramatically harness 2.0 products and leveraged sources. Dramatically drive intermandated expertise rather than clicks-and-mortar benefits.</p>
-<ul class="list-inline social-icons social-icons-small social-icons-square">
-<li><a href="#" class="facebook-bg-hover"><i class="fa fa-facebook"></i></a></li>
-<li><a href="#" class="dribbble-bg-hover"><i class="fa fa-dribbble"></i></a></li>
-<li><a href="#" class="linkedin-bg-hover"><i class="fa fa-linkedin"></i></a></li>
-<li><a href="#" class="twitter-bg-hover"><i class="fa fa-twitter"></i></a></li>
-</ul>
-</div>
-</div>
-</div>
-<div class="col-sm-4">
-<div class="team-member">
-<img src="images/researcher.png" class="img-responsive" alt="Person">
-<div class="member-info">
-<h3 class="name">Yasir Javed</h3>
-<span class="title">Assistant Researcher</span>
-<p class="short-bio">Dramatically harness 2.0 products and leveraged sources. Dramatically drive intermandated expertise rather than clicks-and-mortar benefits.</p>
-<ul class="list-inline social-icons social-icons-small social-icons-square">
-<li><a href="#" class="facebook-bg-hover"><i class="fa fa-facebook"></i></a></li>
-<li><a href="#" class="dribbble-bg-hover"><i class="fa fa-dribbble"></i></a></li>
-<li><a href="#" class="linkedin-bg-hover"><i class="fa fa-linkedin"></i></a></li>
-<li><a href="#" class="twitter-bg-hover"><i class="fa fa-twitter"></i></a></li>
-</ul>
-</div>
-</div>
-</div>
+				<!-- TEAM -->
+				<section>
+					<div class="container">
+						<h2 class="section-heading">Our Team</h2>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="team-member">
+									<!--  <img src="images/researcher.png"  width ="20%" class="img-responsive" alt="Person">  -->
+									<div class="member-info">
+										<h3 class="name">Dr. Anis Koubaa</h3>
+										<span class="title">Director</span>
+										<p class="short-bio keywords">Robotic Software Engineering,
+											Internet-of-Things, Wireless Sensor Networks, Cloud Robotics,
+											Robot Operating System (ROS)</p>
+											<address class="contact-info">
+											<p class="sm">
+												<i class="icon icon_pin_alt ico-styled text-primary "></i>
+												Prince Sultan University, College of Computer and Information Sciences, Office E256 (Old Building), Rafha Street, Saudi Arabia
+											</p>
+											<p class="sm">
+												<i class="icon icon_phone ico-styled text-primary "></i>
+												(11) 494 - 8851
+											</p>
+											<p>
+												<i class="icon icon_mail_alt ico-styled text-primary"></i> <a
+													href="mailto:akoubaa@psu.edu.sa">akoubaa at psu.edu.sa</a>
+											</p>
+										</address>
+										<ul class="list-inline social-icons social-icons-small">
+											<li><a href="#" class="facebook-bg-hover"><i
+													class="fa fa-facebook"></i> </a></li>
+											<li><a href="#" class="dribbble-bg-hover"><i
+													class="fa fa-dribbble"></i> </a></li>
+											<li><a
+												href="https://www.linkedin.com/in/anis-koubaa-84aa4344/"
+												class="linkedin-bg-hover"><i class="fa fa-linkedin"></i> </a>
+											</li>
+											<li><a href="#" class="twitter-bg-hover"><i
+													class="fa fa-twitter"></i> </a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-4">
+								<div class="team-member">
+									<!--  <img src="images/researcher.png"  width ="20%" class="img-responsive" alt="Person">  -->
+									<div class="member-info">
+										<h3 class="name">Dr. Basit Qureshi</h3>
+										<span class="title">Senior Researcher</span>
+										<p class="short-bio keywords">Cloud Computing, Distributed
+											Systems</p>
+											<address class="contact-info">
+											<p class="sm">
+												<i class="icon icon_pin_alt ico-styled text-primary "></i>
+												Prince Sultan University, College of Computer and Information Sciences, Rafha Street, Saudi Arabia
+											</p>
+											<p class="sm">
+												<i class="icon icon_phone ico-styled text-primary "></i>
+												(11) 494 - 8284
+											</p>
+											<p>
+												<i class="icon icon_mail_alt ico-styled text-primary"></i> <a
+													href="mailto:qureshi@psu.edu.sa">qureshi at psu.edu.sa</a>
+											</p>
+										</address>
+										<ul
+											class="list-inline social-icons social-icons-small social-icons-square">
+											<li><a href="#" class="facebook-bg-hover"><i
+													class="fa fa-facebook"></i> </a></li>
+											<li><a href="#" class="dribbble-bg-hover"><i
+													class="fa fa-dribbble"></i> </a></li>
+											<li><a href="#" class="linkedin-bg-hover"><i
+													class="fa fa-linkedin"></i> </a></li>
+											<li><a href="#" class="twitter-bg-hover"><i
+													class="fa fa-twitter"></i> </a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-4">
+								<div class="team-member">
+									<!--  <img src="images/researcher.png"  width ="20%" class="img-responsive" alt="Person">  -->
+									<div class="member-info">
+										<h3 class="name">Yasir Javed</h3>
+										<span class="title">Assistant Researcher</span>
+										<p class="short-bio keywords">Mobile Robots, 5G Wireless
+											Networks, Internet-of-Things, Robot Operating System (ROS)</p>
+										<address class="contact-info">
+											<p class="sm">
+												<i class="icon icon_pin_alt ico-styled text-primary "></i>
+												Prince Sultan University, College of Computer and Information Sciences, Rafha Street, Saudi Arabia
+											</p>
+											<p class="sm">
+												<i class="icon icon_phone ico-styled text-primary "></i>
+												(11) 494 - 8287
+											</p>
+											<p>
+												<i class="icon icon_mail_alt ico-styled text-primary"></i> <a
+													href="mailto:yjaved@psu.edu.sa">yjaved at psu.edu.sa</a>
+											</p>
+										</address>
+										<ul
+											class="list-inline social-icons social-icons-small social-icons-square">
+											<li><a href="#" class="facebook-bg-hover"><i
+													class="fa fa-facebook"></i> </a></li>
+											<li><a href="#" class="dribbble-bg-hover"><i
+													class="fa fa-dribbble"></i> </a></li>
+											<li><a href="#" class="linkedin-bg-hover"><i
+													class="fa fa-linkedin"></i> </a></li>
+											<li><a href="#" class="twitter-bg-hover"><i
+													class="fa fa-twitter"></i> </a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
 
-</div>
+						</div>
 
-<br><br>
-<div class="row">
-<div class="col-sm-4">
-<div class="team-member">
-<img src="images/researcher.png"  class="img-responsive" alt="Person">
-<div class="member-info">
-<h3 class="name">Ms. Maram Alajlan</h3>
-<span class="title">Assistant Researcher</span>
-<p class="short-bio">Dramatically harness 2.0 products and leveraged sources. Dramatically drive intermandated expertise rather than clicks-and-mortar benefits.</p>
-<ul class="list-inline social-icons social-icons-small">
-<li><a href="#" class="facebook-bg-hover"><i class="fa fa-facebook"></i></a></li>
-<li><a href="#" class="dribbble-bg-hover"><i class="fa fa-dribbble"></i></a></li>
-<li><a href="#" class="linkedin-bg-hover"><i class="fa fa-linkedin"></i></a></li>
-<li><a href="#" class="twitter-bg-hover"><i class="fa fa-twitter"></i></a></li>
-</ul>
-</div>
-</div>
-</div>
+						<br> <br>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="team-member">
+									<!--  <img src="images/researcher.png"  width ="20%" class="img-responsive" alt="Person">  -->
+									<div class="member-info">
+										<h3 class="name">Ms. Maram Alajlan</h3>
+										<span class="title">Assistant Researcher</span>
+										<p class="short-bio keywords">Robot Operating System (ROS), Robotics
+											Path Planning, Internet-of-Things, Cloud Robotics</p>
+											<address class="contact-info">
+											<p class="sm">
+												<i class="icon icon_pin_alt ico-styled text-primary "></i>
+												Prince Sultan University, College of Computer and Information Sciences, Rafha Street, Saudi Arabia
+											</p>
+											<p class="sm">
+												<i class="icon icon_phone ico-styled text-primary "></i>
+												(11) 494 - 8172
+											</p>
+											<p>
+												<i class="icon icon_mail_alt ico-styled text-primary"></i> <a
+													href="mailto:majlan@psu.edu.sa">majlan at psu.edu.sa</a>
+											</p>
+										</address>
+										<ul class="list-inline social-icons social-icons-small">
+											<li><a href="#" class="facebook-bg-hover"><i
+													class="fa fa-facebook"></i> </a></li>
+											<li><a href="#" class="dribbble-bg-hover"><i
+													class="fa fa-dribbble"></i> </a></li>
+											<li><a href="#" class="linkedin-bg-hover"><i
+													class="fa fa-linkedin"></i> </a></li>
+											<li><a href="#" class="twitter-bg-hover"><i
+													class="fa fa-twitter"></i> </a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
 
 
-</div>
+						</div>
 
 
 
-</div>
-</section>
-<!-- END TEAM -->
+					</div>
+				</section>
+				<!-- END TEAM -->
 
 
 
 
-</div>
-<!--
+			</div>
+			<!--
 <div class="cta cta-solid-bg cta-2-columns margin-top-50">
 <div class="container">
 <h2 class="heading">An elegant Bootstrap theme with tons of features</h2>
@@ -147,78 +240,85 @@
 </div>
 </div>
 -->
-</div>
-<!-- END PAGE CONTENT -->
-<!-- FOOTER -->
-<footer>
-<div class="container">
-<div class="row">
-<div class="col-md-4">
-<div class="row">
-<div class="col-sm-6">
-<h4 class="footer-heading footer-heading-simple">GENERAL</h4>
-<ul class="list-unstyled link-list">
-<li><a href="#">About us</a></li>
-<li><a href="#">Who we are</a></li>
-<li><a href="#">Career</a></li>
-<li><a href="#">Terms</a></li>
-<li><a href="#">Privacy Policy</a></li>
-</ul>
-</div>
-<div class="col-sm-6">
-<h4 class="footer-heading footer-heading-simple">BUYING GUIDES</h4>
-<ul class="list-unstyled link-list">
-<li><a href="#">How to buy</a></li>
-<li><a href="#">Product Guide</a></li>
-<li><a href="#">License</a></li>
-<li><a href="#">Payment</a></li>
-<li><a href="#">Support</a></li>
-</ul>
-</div>
-</div>
-</div>
-<div class="col-md-8">
-<div class="row">
-<div class="col-md-6">
-<h4 class="footer-heading footer-heading-simple">CONTACT US</h4>
-<address>
-<i class="icon icon_pin_alt ico-styled text-primary"></i> 1234 North Main Street New York, NY 22222
-<br>
-<br>
-<i class="icon icon_phone ico-styled text-primary"></i>
-<abbr title="Phone">P:</abbr> (1800) 765 - 4321
-</address>
-<address><i class="icon icon_mail_alt ico-styled text-primary"></i> <a href="mailto:email@yourdomain.com">email@yourdomain.com</a></address>
-</div>
-<div class="col-md-6">
-<h4 class="footer-heading footer-heading-simple">NEWSLETTER</h4>
-<p>Get the latest update from us by subscribing to our newsletter.</p>
-<form class="newsletter-form" method="post">
-<div class="input-group input-group-lg">
-<input type="email" class="form-control" name="email" placeholder="youremail@domain.com">
-<span class="input-group-btn"><button class="btn btn-primary" type="button"><i class="fa fa-spinner fa-spin"></i><span>SUBSCRIBE</span></button>
-</span>
-</div>
-<div class="alert"></div>
-</form>
-</div>
-</div>
-</div>
-</div>
-</div>
-<p class="copyright-text text-center margin-top-30 no-margin-bottom">&copy;2016 The Develovers. All Rights Reserved.</p>
-</footer>
-<!-- END FOOTER -->
-<div class="back-to-top">
-<a href="#top"><i class="fa fa-chevron-up"></i></a>
-</div>
-</div>
-<!-- END WRAPPER -->
-<!-- JAVASCRIPT -->
-<script src="theme/assets/js/jquery-2.1.1.min.js"></script>
-<script src="theme/assets/js/bootstrap.min.js"></script>
-<script src="theme/assets/js/plugins/easing/jquery.easing.min.js"></script>
-<script src="theme/assets/js/bravana.js"></script>
+		</div>
+		<!-- END PAGE CONTENT -->
+		<!-- FOOTER -->
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="row">
+							<div class="col-sm-6">
+								<h4 class="footer-heading footer-heading-simple">GENERAL</h4>
+								<ul class="list-unstyled link-list">
+									<li><a href="#">About us</a></li>
+									<li><a href="#">Who we are</a></li>
+									<li><a href="#">Career</a></li>
+									<li><a href="#">Terms</a></li>
+									<li><a href="#">Privacy Policy</a></li>
+								</ul>
+							</div>
+							<div class="col-sm-6">
+								<h4 class="footer-heading footer-heading-simple">BUYING GUIDES</h4>
+								<ul class="list-unstyled link-list">
+									<li><a href="#">How to buy</a></li>
+									<li><a href="#">Product Guide</a></li>
+									<li><a href="#">License</a></li>
+									<li><a href="#">Payment</a></li>
+									<li><a href="#">Support</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-8">
+						<div class="row">
+							<div class="col-md-6">
+								<h4 class="footer-heading footer-heading-simple">CONTACT US</h4>
+								<address>
+									<i class="icon icon_pin_alt ico-styled text-primary"></i> 1234
+									North Main Street New York, NY 22222 <br> <br> <i
+										class="icon icon_phone ico-styled text-primary"></i> <abbr
+										title="Phone">P:</abbr> (1800) 765 - 4321
+								</address>
+								<address>
+									<i class="icon icon_mail_alt ico-styled text-primary"></i> <a
+										href="mailto:email@yourdomain.com">email@yourdomain.com</a>
+								</address>
+							</div>
+							<div class="col-md-6">
+								<h4 class="footer-heading footer-heading-simple">NEWSLETTER</h4>
+								<p>Get the latest update from us by subscribing to our
+									newsletter.</p>
+								<form class="newsletter-form" method="post">
+									<div class="input-group input-group-lg">
+										<input type="email" class="form-control" name="email"
+											placeholder="youremail@domain.com"> <span
+											class="input-group-btn"><button class="btn btn-primary"
+												type="button">
+												<i class="fa fa-spinner fa-spin"></i><span>SUBSCRIBE</span>
+											</button> </span>
+									</div>
+									<div class="alert"></div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<p class="copyright-text text-center margin-top-30 no-margin-bottom">&copy;2016
+				The Develovers. All Rights Reserved.</p>
+		</footer>
+		<!-- END FOOTER -->
+		<div class="back-to-top">
+			<a href="#top"><i class="fa fa-chevron-up"></i> </a>
+		</div>
+	</div>
+	<!-- END WRAPPER -->
+	<!-- JAVASCRIPT -->
+	<script src="theme/assets/js/jquery-2.1.1.min.js"></script>
+	<script src="theme/assets/js/bootstrap.min.js"></script>
+	<script src="theme/assets/js/plugins/easing/jquery.easing.min.js"></script>
+	<script src="theme/assets/js/bravana.js"></script>
 
 </body>
 
